87e6cd50b89ef7dcbb2d27d8fbbe66df52e91808
Murat <muratyigit88@gmail.com>
2020-06-21 22:09:30 -0400

    Minor style fixes on the DonationSummary component

--- src/components/DonationSummary.js
+++ src/components/DonationSummary.js
@@ -8,14 +8,14 @@ const DonationSummary = ( {amount, numberOfDonations} ) => {
                 ?
                 <span>Only four days left to fund this project</span>
                 :
-                <span>This project is funded on tiem</span>
+                <span>This project is funded on time</span>
                 }
             </h1>
             { (amount !== 0)
                 ?
-                <span>Join the <span style={{ fontWeight: 'bold'}}>{numberOfDonations !== 0 && numberOfDonations}</span> other donor{numberOfDonations === 1 ? '' : 's'} who already supported this campaign</span>
+                <p className="donor-message">Join the <span style={{ fontWeight: 'bold'}}>{numberOfDonations !== 0 && numberOfDonations}</span> other donor{numberOfDonations === 1 ? '' : 's'} who already supported this campaign</p>
                 :
-                <span>Thanks to all donors who supported this campaign</span>
+                <p className="donor-message">Thanks to all donors who supported this campaign</p>
             }
         </div>
     )
